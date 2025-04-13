b8a9bda69de50017b74ae1fb56a7e44dda55ba99
limory <50734875+limory@users.noreply.github.com>
2025-04-13 23:01:43 +0800

    Update dns-forwarder.sh
    增加logger提示

--- trunk/user/dns-forwarder/dns-forwarder.sh
+++ trunk/user/dns-forwarder/dns-forwarder.sh
@@ -8,10 +8,12 @@ server=$(nvram get dns_forwarder_server)
 
 func_start(){
 	start-stop-daemon -S -b -x $BIN -- -b "$bind_address" -p "$bind_port" -s "$server"
+ 	logger -t "DNSForwarder" "已经启动！"
 }
 
 func_stop(){
 	killall -q dns-forwarder
+ 	logger -t "DNSForwarder" "已经停止！"
 }
 
 case "$1" in

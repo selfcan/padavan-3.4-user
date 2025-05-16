4ec28f727b282892af46bb52b875e69469b398e5
selfcan <99958756+selfcan@users.noreply.github.com>
2025-05-16 08:44:47 +0800

    Update adbyby_host.sh

--- trunk/user/adbyby/adbyby_host.sh
+++ trunk/user/adbyby/adbyby_host.sh
@@ -2,4 +2,9 @@
 #此hosts规则是加载到dnsmasq的。
 #yhost
 https://cdn.jsdelivr.net/gh/VeleSila/yhosts/hosts
-
+https://cdn.jsdelivr.net/gh/sbwml/halflife-list@master/ad-pc.txt
+https://cdn.jsdelivr.net/gh/sbwml/halflife-list@master/ad-mo.txt
+https://cdn.jsdelivr.net/gh/sbwml/halflife-list@master/ad.txt
+https://cdn.jsdelivr.net/gh/sbwml/halflife-list@master/ad2.txt
+https://cdn.jsdelivr.net/gh/sbwml/halflife-list@master/ad3.txt
+https://cdn.jsdelivr.net/gh/sbwml/halflife-list@master/ad-edentw.txt
